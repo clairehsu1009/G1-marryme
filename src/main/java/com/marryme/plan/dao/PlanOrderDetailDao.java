12e1e7f5eafda533e22743e2f9bfa5b603219b4c
clairehsu1009 <clairehsu1009@gmail.com>
2023-08-16 20:26:39 +0800

    planOrderDetail新增dao與daoImpl

---
+++ src/main/java/com/marryme/plan/dao/PlanOrderDetailDao.java
@@ -0,0 +1,9 @@
+package com.marryme.plan.dao;/**
+* ClassName: PlanDetailDao
+* Package: com.marryme.plan.dao
+* Description:
+* @Author  Claire
+* @Create 2023/8/16 下午 05:52
+* @Version 1.0
+*/public interface PlanDetailDao {
+}
---
+++ src/main/java/com/marryme/plan/dao/impl/PlanOrderDetailDaoImpl.java
@@ -0,0 +1,9 @@
+package com.marryme.plan.dao.impl;/**
+* ClassName: PlanOrderDetailDao
+* Package: com.marryme.plan.dao.impl
+* Description:
+* @Author  Claire
+* @Create 2023/8/16 下午 05:54
+* @Version 1.0
+*/public class PlanOrderDetailDao {
+}
